273e814d32b75ab74719c86c1d5f097280e933ee
Ricard Ferrero <r.ferrero.conde@gmail.com>
2022-10-03 07:33:19 +0200

    starting Frontend

--- willwatchproject/willwatchproject/settings.py
+++ willwatchproject/willwatchproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend.apps.FrontendConfig',
     'films.apps.FilmsConfig',
     'genres.apps.GenresConfig',
     'django.contrib.admin',
