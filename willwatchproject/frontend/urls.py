4a4924c635ae4d5dfa2eceb3b7852db3d7438b36
Ricard Ferrero <r.ferrero.conde@gmail.com>
2022-10-22 07:46:00 +0200

    form signup can create users

--- willwatchproject/frontend/urls.py
+++ willwatchproject/frontend/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'frontend'
 urlpatterns = [
 	path('', views.index, name='index'),
-
+	path('signup/', views.signup, name='signup')
 ]
--- willwatchproject/frontend/views.py
+++ willwatchproject/frontend/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-	return render(request, 'frontend/index.html',
-		{'form_creation': UserCreationForm})
+	return render(request, 'frontend/index.html')
+
+def signup(request):
+	# not POST
+	if request.method != 'POST':
+		return HttpResponseRedirect(reverse('frontend:index'))
+
+	# POST from signup form
+	if request.POST['password1'] == request.POST['password2']:
+		user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+		user.save()
+		return render(request, 'frontend/index.html', {'message':'OK'})
+
+	return render(request, 'frontend/index.html', {'message':'Passwords dont match'})
