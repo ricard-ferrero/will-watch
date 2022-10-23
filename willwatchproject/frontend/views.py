a7237530b85d5e7298967614f8709d9f4636b3e3
Ricard Ferrero <r.ferrero.conde@gmail.com>
2022-10-23 08:31:13 +0200

    creating id-cookie

--- willwatchproject/frontend/views.py
+++ willwatchproject/frontend/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+
 from django.contrib.auth.models import User
+from django.contrib.auth import login #cookie login
+
+from django.db import IntegrityError
 
 # Create your views here.
 def index(request):
@@ -17,8 +21,11 @@ def signup(request):
 		try:
 			user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
 			user.save()
+			login(request, user)
 			return render(request, 'frontend/index.html', {'message':'OK', 'type_message':'success'})
-		except:
+		except IntegrityError:
 			return render(request, 'frontend/index.html', {'message':'User allready exists', 'type_message':'danger'})
+		else:
+			return render(request, 'frontend/index.html', {'message':'Some error happened.\nSorry, try again', 'type_message':'warning'})
 
 	return render(request, 'frontend/index.html', {'message':'Passwords do not match', 'type_message':'danger'})
